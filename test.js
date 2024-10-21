6ded4ed32db7fae63a1e929a13669924f84cbf03
kim400im <min2000jae@naver.com>
2024-10-21 19:50:09 +0900

    add pict

---
+++ test.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta-button');
+
+    ctaButton.addEventListener('click', function() {
+        alert("Thanks for your interest! You will be redirected to the download page.");
+    });
+});
